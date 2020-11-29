3791ab253d1e2ea78172d01b0f6bb0ce0d5debfc
Mihai Francu <mishu.francu@gmail.com>
2020-11-29 12:57:48 +0100

    PBD integration testbed

--- PendulumNN.py
+++ PendulumNN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import math
+import random
 
 from torch import nn, optim
 from PendulumProj import compute_new_angle_simple
@@ -22,12 +23,17 @@ class Net(nn.Module):
 torch.manual_seed(0)
 
 PI = 3.1415
+
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = 'cpu'
+print(device)
     
 net = Net(3, 1)
+net.to(device)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=0.0005)
 
-size_train = 2000
+size_train = 20000
 size_valid = 500
 
 def prepare_training_data(set_size):
@@ -46,41 +52,51 @@ def prepare_training_data(set_size):
 def prepare_validation_data(set_size):
     x_valid = torch.rand(set_size, 3)
     y_valid = torch.empty(set_size)
+    dx = random.random()
+    dy = random.random()
+    print(dx, dy)
     for i in range(0, set_size):
         angle = PI * i / set_size
-        x_valid[i, 0] = angle / PI
-        x_valid[i, 1] = 0.1
-        x_valid[i, 2] = 0.1
-        dx = 0.1
-        dy = 0.1
+        x_valid[i, 0] = angle / PI #normalize in the interval (0, pi)
+        x_valid[i, 1] = dx
+        x_valid[i, 2] = dy
         out = (compute_new_angle_simple(angle, dx, dy) + PI / 2) / PI # normalize in the interval (-pi/2, pi/2)
         y_valid[i] = out - x_valid[i, 0]
     return x_valid, y_valid
 
 # prepare training set
 x_train, y_train = prepare_training_data(size_train)
+x_train = x_train.to(device)
+y_train = y_train.to(device)
 
 # prepare validation set
 x_valid, y_valid = prepare_validation_data(size_valid)
+x_valid = x_valid.to(device)
+y_valid = y_valid.to(device)
 
 # fig, ax = plt.subplots()
 # ax.scatter(PI * x_train, PI * y_train)
 # fig.savefig("fun.png")
 
-# train the NN
-for epoch in range(10000):
-    y_pred = net(x_train)
-    y_pred = torch.squeeze(y_pred)
-    train_loss = criterion(y_pred, torch.squeeze(y_train))
-    if epoch % 1000 == 0:
-        print(train_loss.item())
-    optimizer.zero_grad()
-    train_loss.backward()
-    optimizer.step()
+train = False
+path = "PendNet.dat"
+if train:
+    # train the NN
+    for epoch in range(10000):
+        y_pred = net(x_train)
+        y_pred = torch.squeeze(y_pred)
+        train_loss = criterion(y_pred, torch.squeeze(y_train))
+        if epoch % 1000 == 0:
+            print(train_loss.item())
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+        
+    print(train_loss.item())
+    torch.save(net.state_dict(), path)
+else:
+    net.load_state_dict(torch.load(path))
     
-print(train_loss.item())
-torch.save(net.state_dict(), "PendNet.dat")
-
 # validate the NN
 y_pred = net(x_valid)
 print(y_pred[0], y_pred[10])
@@ -89,8 +105,11 @@ valid_loss = criterion(y_pred.view(-1), y_valid)
 print(valid_loss.item())
     
 fig, ax = plt.subplots()
-ax.scatter(PI * x_valid[:,0], PI * y_pred.detach().numpy())
-ax.scatter(PI * x_valid[:,0], PI * y_valid)
+x_valid_cpu = x_valid.cpu()
+ax.scatter(PI * x_valid_cpu[:,0], PI * y_pred.detach().cpu().numpy())
+ax.scatter(PI * x_valid_cpu[:,0], PI * y_valid.cpu())
 fig.savefig("pred.png")
+# fig.show()
+# input()
 
 
---
+++ PendulumSim.py
@@ -0,0 +1,65 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+from PendulumProj import compute_new_angle_simple
+from PendulumNN import net
+
+def project(x, y):
+    len = math.sqrt(x * x + y * y)
+    x1 = x * l / len
+    y1 = y * l / len
+    return x1, y1
+
+# simple integration using PBD
+
+l = 1 # pendulum (rest) length
+PI = 3.1415
+
+# initial conditions
+theta0 = 0.0
+x0 = l * math.cos(theta0)
+y0 = l * math.sin(theta0)
+theta0dot = 0.2
+x0dot = -l * theta0dot * math.sin(theta0)
+y0dot = l * theta0dot * math.cos(theta0)
+
+dt = 1 # time step in seconds
+steps = 20
+x = np.empty(steps)
+y = np.empty(steps)
+x[0] = x0;
+y[0] = y0
+xdot = np.empty(steps)
+ydot = np.empty(steps)
+xdot[0] = x0dot;
+ydot[0] = y0dot
+for i in range(0, steps - 1):
+    x1 = x[i] + dt * xdot[i]
+    y1 = y[i] + dt * ydot[i]
+    theta0 = math.atan2(y[i], x[i])    
+    theta1 = compute_new_angle_simple(theta0, dt * xdot[i], dt * ydot[i])
+    args = torch.tensor([theta0 / PI, dt * xdot[i], dt * ydot[i]], dtype=torch.float)
+    theta1net = net(args) * PI - PI / 2
+    #print(dt * xdot[i], dt * ydot[i])
+    print(theta1, theta1net.item() + theta0)
+    #theta1 = theta0 + theta1net;
+    x[i+1] = l * math.cos(theta1)
+    y[i+1] = l * math.sin(theta1)
+    # x2, y2 = project(x1, y1)
+    # x[i+1] = x2
+    # y[i+1] = y2
+    xdot[i+1] = (x[i+1] - x[i]) / dt
+    ydot[i+1] = (y[i+1] - y[i]) / dt
+
+fig, ax = plt.subplots()
+plt.axis('square')
+plt.xlim(-1.1, 1.1)
+plt.ylim(-1.1, 1.1)
+ax.set_aspect('equal')
+ax.scatter(x, y)
+fig.savefig("sim.png")
+fig.show()
+input()
+    
